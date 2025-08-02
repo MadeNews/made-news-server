3be816f1865a381ab38f9235ee702d35428ba98f
MadeNews <themadenewscompany@gmail.com>
2025-08-02 17:57:14 +0500

    removed error caused by cron dependency

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const cronJob = require("./cron/weeklyJob");
 const apiRoutes = require("./routes/apiRoutes");
 const publicRoutes = require("./routes/publicRoutes");
 const authMiddleware = require("./middleware/authMiddleware");
